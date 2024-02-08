1d0f6e8cf6984dff00b3f0b81e7b083aad4e7160
meletulu <meletulu@yahoo.com>
2024-02-07 19:41:01 -0800

    header and footer

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 
+import Home from "./pages/Home/Home";
+
 function App() {
   return (
     <div className="App">
-      <h1>Netflix Clone</h1>
+      <Home />
     </div>
   );
 }
