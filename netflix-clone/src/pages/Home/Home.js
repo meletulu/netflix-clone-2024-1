ef7ad81a8d7cfebc3bcd2f43746bbc0e3bb9da59
meletulu <meletulu@yahoo.com>
2024-02-13 16:27:21 -0800

    row

--- netflix-clone/src/pages/Home/Home.js
+++ netflix-clone/src/pages/Home/Home.js
@@ -2,12 +2,16 @@ import React from "react";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 import Banner from "../../components/Banner/Banner";
+import RowList from "../../components/Rows/RowList/RowList";
+// import Nav from "../../components/Nav/Nav";
 
 const Home = () => {
   return (
     <>
       <Header />
+      {/* <Nav /> */}
       <Banner />
+      <RowList />
       <Footer />
     </>
   );
--- netflix-clone/src/utils/axios.js
+++ netflix-clone/src/utils/axios.js
@@ -4,4 +4,11 @@ const instance = axios.create({
   baseURL: "https://api.themoviedb.org/3",
 });
 
+// (async () => {
+//   let URL = await instance.get(
+// // // "/movie/top_rated?api_key=25ac4dbfffbbdaefa9c5c5eb2e25feec"
+//   );
+//   console.log(URL.data.results);
+// })();
+
 export default instance;
